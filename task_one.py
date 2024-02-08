2119c2a2f273b1c25676c9517290b6f71e63a9c2
Serhii-Kozchenko <serhii7534@i.ua>
2024-02-08 16:31:44 +0200

    add task_four

--- task_one.py
+++ task_one.py
@@ -15,16 +15,16 @@
 
 
 def caching_fibonacci():
-    cache = {}
+    cache = {}   # Створення словника для зберігання кешу
     def fibonacci(n):
-        if n <=0:
-            return 0
-        elif n==1:
-            return 1
-        elif n in cache:
-            return cache[n]
+        if n <=0:  
+            return 0 
+        elif n==1:    
+            return 1   
+        elif n in cache:   
+            return cache[n] # Якщо число вже обраховувалось повернення його з кешу
         else:
-            cache[n] = fibonacci(n - 1) + fibonacci(n - 2)
+            cache[n] = fibonacci(n - 1) + fibonacci(n - 2) # Розрахунок числа якого немає в кеші 
             return cache[n]
     return fibonacci
 
